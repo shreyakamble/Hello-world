edf1f577f866127bcf27d9335564724339a62731
shreyakamble <30909480+shreyakamble@users.noreply.github.com>
2017-08-14 21:46:10 -0700

    Add files via upload

---
+++ APCSA/Count/Count/Count.java
@@ -0,0 +1,22 @@
+//***********************************************************
+// Name: Shreya Kamble
+// Program: Count
+// Function: counts from 1 to 5 in three different languages
+// **********************************************************
+public class Count{
+    public static void main (String[] args){
+        
+        //English
+        System.out.println("one two three four five");
+        
+        //French
+        System.out.println("un deux trois quatre cinq");
+        
+        //Spanish
+        System.out.println("uno dos thres quatro cinco");
+    }
+
+}
+//2. The program is easier to read with blank lines that separate the comments and printlns.
+//3. There are two error messages: illegal start of expression, and ';' expected.
+//4. Putting a comment within a comment does not cause any programs in the code.
---
+++ APCSA/Simple/Simple/Simple.java
@@ -0,0 +1,19 @@
+//************************************************
+// Simple.java
+//
+// Print a simple message about Java.
+//*************************************************
+
+public class Simple{
+    public static void main (String[] args){
+        System.out.println ("Java rocks!!");
+    }
+
+}
+//1. simple: yes, legal identifier. No, not a good name. A capital letter would be better for style.
+//2. SimpleProgram: yes, legal identifier. Yes, a good name.
+//3. 1Simple: No, not a legal identifier. It starts with a number.
+//4. _Simple_: yes, legal identifier. Yes, a good name.
+//5. *Simple*: no, not a legal identifier. It uses invalid character '*'
+//6. $123_45: yes, legal identifier. No, not a good name. It does not describe the program.
+//7. Simple!: no, not a legal identifier. It uses invalid character '!'
