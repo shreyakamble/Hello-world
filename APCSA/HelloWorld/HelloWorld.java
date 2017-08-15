b0ba5397729c25575e0a9a51b2438892f010a011
shreyakamble <30909480+shreyakamble@users.noreply.github.com>
2017-08-15 11:04:10 -0700

    Update HelloWorld.java

--- APCSA/HelloWorld/HelloWorld.java
+++ APCSA/HelloWorld/HelloWorld.java
@@ -5,7 +5,6 @@ public class HelloWorld
         System.out.println ("Hello, World!");
     }
 }
-
 //6. There was no error message after deleting an 'l' from the class name.
 //7. There is no error message because only the input in the code outputs the same message. There was no error in the code because only the input message was changed. The message has now changed from "Hello, World!" to "Helo, World!" 
 //8. There are three error messages total: unclosed string literal, ';' expected, and reached end of file while parsing.
