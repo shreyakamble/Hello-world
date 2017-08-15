fe8fcce99eda252ed3b3be8cf6411c3e822ace0c
shreyakamble <30909480+shreyakamble@users.noreply.github.com>
2017-08-15 11:07:06 -0700

    Update Poem.java

--- APCSA/Poem/Poem/Poem.java
+++ APCSA/Poem/Poem/Poem.java
@@ -1,11 +1,10 @@
-public class Poem {
+public class Poem 
+{
     public static void main (String[] args)
     {
         System.out.println("Roses are red");
         System.out.println("Violets are blue");
         System.out.println("Sugar is sweet");
-        System.out.println("And so are you!");
-
-        
+        System.out.println("And so are you!"); 
     }
 }
