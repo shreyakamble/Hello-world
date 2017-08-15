f1f386f8cd116e86d77a217840b169f5d351effa
shreyakamble <30909480+shreyakamble@users.noreply.github.com>
2017-08-15 11:08:13 -0700

    Update Problems.java

--- APCSA/Problems/Problems/Problems.java
+++ APCSA/Problems/Problems/Problems.java
@@ -4,14 +4,15 @@
 //
 //**********************************************************
 
-public class Problems{
-    public static void main (String[] args){
+public class Problems
+{
+    public static void main (String[] args)
+    {
         System.out.println ("!!!!!!!!!!!!!!!!!!!!!!!");
         System.out.println ("This program used to have lots of problems,");
         System.out.println ("but if it prints this, you fixed them all.");
         System.out.println ("         *** Hurray! ***");
         System.out.println ("!!!!!!!!!!!!!!!!!!!!!!");
-
     }
 
 }
